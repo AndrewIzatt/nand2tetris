c91ef5e6082943facea4459ff49066e8e4018163
Andrew Izatt <izatt.andrew@gmail.com>
2024-02-21 17:23:53 -0700

    updating

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen
-// by writing 'black' in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen by writing
-// 'white' in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
-
-//// Replace this comment with your code.
-=======
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
@@ -85,4 +69,3 @@ M=0
 
 
 
->>>>>>> 3184509b3b90df5843c2b3cccf43fee134882202
