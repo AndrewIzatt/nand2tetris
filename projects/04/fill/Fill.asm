8d367b57268b4f8a520e2a34fa134d259668069a
AndrewIzatt <izatt.andrew@gmail.com>
2024-03-17 16:13:00 -0600

    Final Fill.asm program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,23 +9,48 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+@8192
+D=A
+@max
+M=D
 @position
 M=0
-(INFINITE) // Infinite Loop
-// Check Value of Keyboard Input
-@KBD
-D=M
-@PRESSED1
-D;JGT
-// Scenarios
-// 1. A key is pressed
-    (PRESSED1) // Outermost Loop
-    // 2. A key is pressed and then released 
-        // Or 3. No key is pressed
-        // Is there any pixels filled in with black?
-        // Yes?
-            // Find Last Most Pixel and Clear it
-        // No?
-            // Go back to INFINITE Loop 
-@INFINITE
-0;JMP
+(LOOP)
+    @KBD // Check if key is pressed
+    D=M
+    @FILL // If key is presed, fill the screen   
+    D;JGT
+    @position // If it gets to here, we automatically know that no key is pressed
+    D=M // Check if screen is filled in at all
+    @ERASE
+    D;JGE
+    @NOTPRESSED
+    0;JMP
+(FILL)
+    @SCREEN // A = 16384
+    D=A
+    @position // Select position counter
+    A=D+M // A = 16384 + position
+    M=-1 // Set memory at that location to -1
+    // Check if screen is totally filled
+    @max
+    D=M
+    @position
+    D=D-M
+    @LOOP // If screen is totally filled (D=0), restart loop (nothing will get filled in anymore, even if pressed)
+    D;JEQ
+    @position // select position
+    M=M+1 // add 1 to position memory
+    @LOOP // restart loop
+    0;JMP
+(ERASE)
+    @SCREEN
+    D=A
+    @position
+    A=D+M // This can't be bigger that 24577
+    M=0
+    @position
+    M=M-1
+(NOTPRESSED)
+    @LOOP
+    0;JMP
