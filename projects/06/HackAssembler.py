00f46b9af02d19b6e3e671e214ecb1abbbff0c14
AndrewIzatt <izatt.andrew@gmail.com>
2024-03-30 15:03:48 -0600

    Test function scope

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -1,8 +1,12 @@
 import sys
 import fileinput
 
-def main():
-    
+def hello_world():
+    print("Hello World")
+
+def main(): 
+    print("Hello World")    
+    hello_world()
 
 if __name__ == "__main__":
     main()
