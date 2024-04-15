c8f199fce45c7266c40fbb4c4fcc7ce40bf7ae61
AndrewIzatt <izatt.andrew@gmail.com>
2024-04-14 18:22:02 -0600

    read input output (2024-04-14)

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -1,12 +1,16 @@
 import sys
 import fileinput
 
-def hello_world():
-    print("Hello World")
+def parser(input_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        read_data = f.read()
+        print(read_data)
 
-def main(): 
-    print("Hello World")    
-    hello_world()
+    
+
+def main():
+    parser(sys.argv[1]) 
+    
 
 if __name__ == "__main__":
     main()
