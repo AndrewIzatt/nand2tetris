f1886056f4192c49a56366c29a56a8b0073ca0cf
AndrewIzatt <izatt.andrew@gmail.com>
2024-04-21 14:03:41 -0600

    Refactor Parser class to read input file and improve code suggestions in settings.json

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -1,19 +1,18 @@
 import sys
 import fileinput
 
-class Parser(self, input_file):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        read_data = f.read()
-        print(read_data)
-
-    # def hasMoreLines(self, input_file):
-
+class Parser:
+    def __init__(self, input_file):
+        self.file = open(input_file, 'r', encoding='utf-8')
+        
+    def hasMoreLines(self):
+        print(self.file.readline())
 
     
 
 def main():
-    hack = Parser()
-    hack.hasMoreLines(sys.argv[1]) 
+    hack = Parser("test.asm")
+    hack.hasMoreLines()
     
 
 if __name__ == "__main__":
