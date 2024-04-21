321936725732759ff219e93a3f5422d692e4d58b
AndrewIzatt <izatt.andrew@gmail.com>
2024-04-20 20:34:50 -0600

    change Parser into class

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -1,15 +1,18 @@
 import sys
 import fileinput
 
-def parser(input_file):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        read_data = f.read()
-        print(read_data)
+class Parser:
+
+    def hasMoreLines(input_file):
+        with open(input_file, 'r', encoding='utf-8') as f:
+            read_data = f.read()
+            print(read_data)
 
     
 
 def main():
-    parser(sys.argv[1]) 
+    f = Parser()
+    f.hasMoreLines(sys.argv[1]) 
     
 
 if __name__ == "__main__":
