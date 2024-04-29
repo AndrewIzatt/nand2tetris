1474fe2ccbfdb342bf20e3da4df386a16d71b910
AndrewIzatt <izatt.andrew@gmail.com>
2024-04-28 18:01:11 -0600

    Refactor Parser class to remove unnecessary line in test.asm and improve code readability

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -6,14 +6,22 @@ class Parser:
         self.file_path = file_path
 
     def parse(self):
+        
         with open(self.file_path, 'r') as file:
             for line in file:
                 if '//' in line:
+                    print("Before splitting: ", line)
                     line = line.split('//')[0]
+                    #line = line.split('//')[0]
+                    print("After split: ", line)
+                    print("This line is of type: ", type(line))
                 line = line.strip()
+                print("Stripped line: ", line)
+                print("The line is of type: ", type(line))
+                print("The line's bool value is: ", bool(line))
                 if line:
                     print(line)
 
 if __name__ == "__main__":
-    parser = Parser(sys.argv[1])
-    parser.parse()
+    hack = Parser(sys.argv[1])
+    hack.parse()
--- projects/06/test.asm
+++ projects/06/test.asm
@@ -1,5 +1,7 @@
-This is a test.
-// New line that shouldn't print.
-This is the third line. // Ignore this part
-This is the fourth line.
-This is the fifth line.
+This is a test. (1)
+// New line that shouldn't print. (2)
+This is the third line. (3) // Ignore this part
+This is the fourth line. (4)
+This is the fifth line. (5)
+
+There should be 6 lines even though there was seven originally. (7)
