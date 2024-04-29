d6cc14b4d8b400d54bc5e117ea649fa44ac55c28
AndrewIzatt <izatt.andrew@gmail.com>
2024-04-28 22:21:12 -0600

    refactor HackAssembler and edit error in test.asm

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -1,27 +1,25 @@
 import os
 import sys
+import io
 
 class Parser:
     def __init__(self, file_path):
-        self.file_path = file_path
+        self.file = io.BufferedReader(open(file_path, 'rb'))
 
-    def parse(self):
-        
-        with open(self.file_path, 'r') as file:
-            for line in file:
-                if '//' in line:
-                    print("Before splitting: ", line)
-                    line = line.split('//')[0]
-                    #line = line.split('//')[0]
-                    print("After split: ", line)
-                    print("This line is of type: ", type(line))
-                line = line.strip()
-                print("Stripped line: ", line)
-                print("The line is of type: ", type(line))
-                print("The line's bool value is: ", bool(line))
-                if line:
-                    print(line)
+    def hasMoreLines(self):
+        return bool(self.file.peek(1))
+
+    def advance(self):
+            line = self.file.readline().decode('utf-8')
+            if '//' in line:
+                line = line.split('//')[0]
+            line = line.strip()
+            if line:
+                print(line)
 
 if __name__ == "__main__":
-    hack = Parser(sys.argv[1])
-    hack.parse()
+    # Remember to redo this
+    hack = Parser("/home/orca/Documents/Coding/nand2tetris/projects/06/test.asm")
+    while hack.hasMoreLines():
+        hack.advance()
+    print("This is the end.")
--- projects/06/test.asm
+++ projects/06/test.asm
@@ -4,4 +4,4 @@ This is the third line. (3) // Ignore this part
 This is the fourth line. (4)
 This is the fifth line. (5)
 
-There should be 6 lines even though there was seven originally. (7)
+There should be 5 lines even though there was seven originally. (7)
