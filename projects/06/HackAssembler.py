f2103de7cb521b88af5334d95cccc061bc7acd5b
AndrewIzatt <izatt.andrew@gmail.com>
2024-05-06 09:23:38 -0600

    Refactor Parser class to handle different instruction types and write to output file

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -15,11 +15,22 @@ class Parser:
                 line = line.split('//')[0]
             line = line.strip()
             if line:
-                print(line)
+                return line
+    
+    def instructionType(self, input):
+            if input[0] == "@":
+                return "A_INSTRUCTION\n"
+            elif input[0] == "T": # Just for testing, everything should be a C_Instruction
+                return "C_INSTRUCTION\n"
+            else:
+                return "L_INSTRUCTION\n" # Or an L Instruction
 
 if __name__ == "__main__":
     # Remember to redo this
     hack = Parser("/home/orca/Documents/Coding/nand2tetris/projects/06/test.asm")
+    output = open("/home/orca/Documents/Coding/nand2tetris/projects/06/test.hack", "w")
     while hack.hasMoreLines():
-        hack.advance()
-    print("This is the end.")
+        instruction = hack.advance()
+        if instruction is not None:
+            instruction = hack.instructionType(instruction)
+            output.write(instruction)
