f158a4811a138e121027122131192db28bc06ab8
AndrewIzatt <izatt.andrew@gmail.com>
2024-09-07 20:49:31 -0600

    changes

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -1,45 +1,47 @@
-import os
-import sys
 import io
+print("This is a string")
 
 class Parser:
     def __init__(self, file_path):
-        self.file = io.BufferedReader(open(file_path, 'rb'))
+        self.file = io.BufferedReader(open(file_path, "rb"))
 
     def hasMoreLines(self):
         return bool(self.file.peek(1))
 
     def advance(self):
-            line = self.file.readline().decode('utf-8')
-            if '//' in line:
-                line = line.split('//')[0]
-            line = line.strip()
-            if line:
-                return line
-    
+        line = self.file.readline().decode("utf-8")
+        if "//" in line:
+            line = line.split("//")[0]
+        line = line.strip()
+        if line:
+            return line
+
     def instructionType(self, input):
-            if input[0] == "@":
-                return "A_INSTRUCTION\n"
-            elif "=" in input or ";" in input: # Just for testing, everything should be a C_Instruction
-                return "C_INSTRUCTION\n"
-            elif input[0] == "(":
-                return "L_INSTRUCTION\n" # Or an L Instruction
-            else:
-                 raise SyntaxError("Invalid Instruction")
-    
+        if input[0] == "@":
+            return "A_INSTRUCTION\n"
+        elif (
+            "=" in input or ";" in input
+        ):  # Just for testing, everything should be a C_Instruction
+            return "C_INSTRUCTION\n"
+        elif input[0] == "(":
+            return "L_INSTRUCTION\n"  # Or an L Instruction
+        else:
+            raise SyntaxError("Invalid Instruction")
+
     def symbol(self, symbolic_instruction):
-         # Skipping Labels for right now
-         print(f"The symbolic instruction: {symbolic_instruction}")
-         return symbolic_instruction[1:]
-    
+        # Skipping Labels for right now
+        print(f"The symbolic instruction: {symbolic_instruction}")
+        return symbolic_instruction[1:]
+
     def dest(self, dest_instruction):
-         pass
-    
+        pass
+
     def comp(self, comp_instruction):
-         pass
-         
+        pass
+
     def jump(self, jump_instruction):
-         pass
+        pass
+
 
 if __name__ == "__main__":
     # Remember to redo this
@@ -50,5 +52,7 @@ if __name__ == "__main__":
         if instruction is not None:
             instruction_type = hack.instructionType(instruction)
             if instruction_type[0] == "A" or instruction_type[0] == "L":
-                parsed_instruction = hack.symbol(instruction) 
-                output.write(parsed_instruction + "\n") # Move out of "If" when finished
+                parsed_instruction = hack.symbol(instruction)
+                output.write(
+                    parsed_instruction + "\n"
+                )  # Move out of "If" when finished
